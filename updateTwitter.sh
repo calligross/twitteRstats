c2320d52cf684a94517cf7dc7dfe343b51c0d966
calli <calli@no-public-mail.com>
2016-04-30 12:29:30 +0200

    Added update script

---
+++ updateTwitter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~/R/twitteRstats
+git pull origin master
+R CMD BATCH updatedb.R 
+git add tweets.db
+git commit -m 'updated tweets.db'
+git push origin master
+cd ../Blog
+git pull origin master
+cp ../twitteRstats/twitteRstats.md .
+cp ../twitteRstats/figure/twitterstats-* figure/
+git add figure/twitterstats-*
+git add twitteRstats.md 
+git commit -m "Udpated twitterstats"
+git push origin master
