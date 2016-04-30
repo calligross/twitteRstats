2989971da990ecbf1ea9821741852270491ca2c6
Calli Gross <m.gross@mac.com>
2016-04-30 11:21:17 +0200

    initial commit

---
+++ updatedb.R
@@ -0,0 +1,23 @@
+library(twitteR)
+library(knitr)
+source('~mg/twitter.auth')
+blog_dir <- '~mg/Documents/R/test/'
+
+setup_twitter_oauth(consumer_key = consumer_key,
+                    consumer_secret = consumer_secret,
+                    access_token = access_token,
+                    access_secret = access_secret)
+
+register_sqlite_backend('tweets.db')
+tweets <- load_tweets_db(as.data.frame = TRUE)
+tweets_new <- Rtweets(sinceID = max(tweets$id), n = 2000)
+store_tweets_db(tweets_new)
+
+# update the posting
+
+knitr::knit(input = 'twitteRstats.Rmd', output = 'twitteRstats.md', quiet = TRUE)
+
+files <- c('twitteRstats.md', list.files(path = 'figure', full.names = TRUE)) 
+
+# the blog_dir and a subdirectory figure need to exist!
+file.copy(from = files, to = paste0(blog_dir, files), overwrite = TRUE)
